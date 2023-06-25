126c6172d3a5bc7f1e23fd5679764fbc90f7eb6b
Scott Fudally <nychthemeron@live.com>
2023-06-25 16:32:16 -0700

    Updates for STM32 Nucleu F446RE

--- rtic_v1/stm32f3_blinky/memory.x
+++ rtic_v1/stm32f3_blinky/memory.x
@@ -1,5 +1,8 @@
+
 MEMORY
 {
-    FLASH : ORIGIN = 0x08000000, LENGTH = 256K
-    RAM : ORIGIN = 0x20000000, LENGTH = 40K
+  /* Flash memory at 0x0800 0000 - 0x081F FFFF, range 2MB and has a size of 512kB*/
+  FLASH : ORIGIN = 0x08000000, LENGTH = 512K
+  /* RAM begins at 0x20000000 and has a size of 128kB*/
+  RAM : ORIGIN = 0x20000000, LENGTH = 128K
 }
--- rtic_v1/stm32f3_blinky/src/main.rs
+++ rtic_v1/stm32f3_blinky/src/main.rs
@@ -6,11 +6,11 @@
 use panic_rtt_target as _;
 use rtic::app;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32f3xx_hal::gpio::{Output, PushPull, PA5};
-use stm32f3xx_hal::prelude::*;
+use stm32f4xx_hal::gpio::*;
+use stm32f4xx_hal::prelude::*;
 use systick_monotonic::{fugit::Duration, Systick};
 
-#[app(device = stm32f3xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
+#[app(device = stm32f4xx_hal::pac, peripherals = true, dispatchers = [EXTI1])]
 mod app {
     use super::*;
 
@@ -21,54 +21,63 @@ mod app {
     struct Local {
         led: PA5<Output<PushPull>>,
         state: bool,
+        button: PC13<Input>,
     }
 
     #[monotonic(binds = SysTick, default = true)]
     type MonoTimer = Systick<1000>;
 
     #[init]
-    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
-        // Setup clocks
-        let mut flash = cx.device.FLASH.constrain();
-        let mut rcc = cx.device.RCC.constrain();
+    fn init(mut cx: init::Context) -> (Shared, Local, init::Monotonics) {
 
-        let mono = Systick::new(cx.core.SYST, 36_000_000);
+        let gpioa = cx.device.GPIOA.split();
+        let mut led = gpioa.pa5.into_push_pull_output();
+
+        // Set up the system clock. We want to run at 48MHz for this one.
+        let rcc = cx.device.RCC.constrain();
+        let _clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
+
+        let mono = Systick::new(cx.core.SYST, 48_000_000);
+
+        let mut syscfg = cx.device.SYSCFG.constrain();
+        let mut button = cx.device.GPIOC.split().pc13.into_pull_down_input();
+        button.make_interrupt_source(&mut syscfg);
+        button.enable_interrupt(&mut cx.device.EXTI);
+        button.trigger_on_edge(&mut cx.device.EXTI, Edge::Falling);
 
         rtt_init_print!();
         rprintln!("init");
 
-        let _clocks = rcc
-            .cfgr
-            .use_hse(8.MHz())
-            .sysclk(36.MHz())
-            .pclk1(36.MHz())
-            .freeze(&mut flash.acr);
-
         // Setup LED
-        let mut gpioa = cx.device.GPIOA.split(&mut rcc.ahb);
-        let mut led = gpioa
-            .pa5
-            .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
-        led.set_high().unwrap();
+        led.set_high();
 
         // Schedule the blinking task
         blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
 
         (
             Shared {},
-            Local { led, state: false },
+            Local { led, state: false, button },
             init::Monotonics(mono),
         )
+    }
+
+    /// React on the button click
+    // see here for why this is EXTI15_10: https://github.com/stm32-rs/stm32f4xx-hal/blob/6d0c29233a4cd1f780b2fef3e47ef091ead6cf4a/src/gpio/exti.rs#L8-L23
+    #[task(binds = EXTI15_10, local = [button])]
+    fn button_click(ctx: button_click::Context) {
+        ctx.local.button.clear_interrupt_pending_bit();
+        cortex_m_semihosting::hprintln!("button!");
     }
 
     #[task(local = [led, state])]
     fn blink(cx: blink::Context) {
         rprintln!("blink");
+        cortex_m_semihosting::hprintln!("blink!");
         if *cx.local.state {
-            cx.local.led.set_high().unwrap();
+            cx.local.led.set_high();
             *cx.local.state = false;
         } else {
-            cx.local.led.set_low().unwrap();
+            cx.local.led.set_low();
             *cx.local.state = true;
         }
         blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
